c2b2c42219e1e8c78239dede9f52c2e0cd7c471e
lejeunel <me@lejeunel.org>
2024-06-29 15:22:35 +0200

    floating calendar and bluetooth

--- desktop/i3/default.nix
+++ desktop/i3/default.nix
@@ -1,159 +1,217 @@
 { config, lib, pkgs, font, nix-colors, ... }:
+  let
+    term_float = "/usr/bin/alacritty --class floating_shell -e";
+    calendar = "${term_float}" + " calcurse";
+    bluetooth = "${term_float}" + " bluetuith";
+    bluetooth_mac_addr = "/usr/bin/bluetoothctl list | cut -d\  -f2";
 
-{
+in {
 
-  xsession.windowManager.i3 = {
-    enable = true;
-    package = pkgs.i3-gaps;
-    config = rec {
-      bars = [
-        {
-          fonts = {
-            names = ["${font}"];
-            size = 12.0;
 
-          };
-          position = "top";
-          statusCommand = "/usr/bin/i3status-rs /home/laurent/.config/i3status-rust/config-top.toml";
-          colors = rec {
-                        background = "#${config.colorScheme.palette.base00}";
-                        focusedBackground = "#${config.colorScheme.palette.base00}";
-                        statusline = "#${config.colorScheme.palette.base00}";
-                        focusedStatusline = statusline;
-                        bindingMode = rec {
-                          inherit background;
-                          border = "#${config.colorScheme.palette.base08}";
-                          text = border;
-                        };
-                        focusedWorkspace = {
-                          background = "#${config.colorScheme.palette.base02}";
-                          border = "#${config.colorScheme.palette.base08}";
-                          text = "#${config.colorScheme.palette.base08}";
-                        };
-                        activeWorkspace = {
-                          inherit background;
-                          inherit (focusedWorkspace) text border;
-                        };
-                        inactiveWorkspace = rec {
-                          inherit background;
-                          border = "#${config.colorScheme.palette.base05}";
-                          text = border;
-                        };
-                      };
-        }
-
-      ];
-
-      fonts = {
-        names = ["${font}"];
-        size = 12.0;
-
-      };
-      modifier = "Mod1";
-      gaps = {
-        inner = 10;
-        outer = 5;
-      };
-      colors = {
-        focused = {
-          border = "#${config.colorScheme.palette.base00}";
-          background = "#${config.colorScheme.palette.base01}";
-          text = "#${config.colorScheme.palette.base07}";
-          indicator = "#${config.colorScheme.palette.base00}";
-          childBorder = "#${config.colorScheme.palette.base08}";
-        };
-        unfocused = {
-          border = "#${config.colorScheme.palette.base00}";
-          background = "#${config.colorScheme.palette.base00}";
-          text = "#${config.colorScheme.palette.base03}";
-          indicator = "#${config.colorScheme.palette.base00}";
-          childBorder = "#${config.colorScheme.palette.base00}";
-        };
-        focusedInactive = {
-          border = "#${config.colorScheme.palette.base00}";
-          background = "#${config.colorScheme.palette.base00}";
-          text = "#${config.colorScheme.palette.base0F}";
-          indicator = "#${config.colorScheme.palette.base00}";
-          childBorder = "#${config.colorScheme.palette.base00}";
-        };
-        urgent = {
-          border = "#${config.colorScheme.palette.base00}";
-          background = "#${config.colorScheme.palette.base00}";
-          text = "#${config.colorScheme.palette.base04}";
-          indicator = "#${config.colorScheme.palette.base00}";
-          childBorder = "#${config.colorScheme.palette.base00}";
-        };
-      };
-      keybindings = lib.mkOptionDefault {
-        "XF86AudioMute" = "exec amixer set Master toggle";
-        "XF86AudioLowerVolume" = "exec amixer set Master 4%-";
-        "XF86AudioRaiseVolume" = "exec amixer set Master 4%+";
-        "XF86MonBrightnessDown" = "exec brightnessctl set 4%-";
-        "XF86MonBrightnessUp" = "exec brightnessctl set 4%+";
-        "${modifier}+Return" = "exec /usr/bin/alacritty";
-        "${modifier}+e" = "exec /usr/bin/emacsclient -nc";
-        "${modifier}+Shift+d" = "exec /usr/bin/rofi -show window";
-        "${modifier}+d" = "exec /usr/bin/rofi -modi drun -show drun";
-        "${modifier}+Shift+e" = "exec systemctl suspend";
-
-        "${modifier}+q" = "kill";
-
-        "${modifier}+h" = "focus left";
-        "${modifier}+j" = "focus down";
-        "${modifier}+k" = "focus up";
-        "${modifier}+l" = "focus right";
-
-        "${modifier}+Shift+h" = "move left";
-        "${modifier}+Shift+j" = "move down";
-        "${modifier}+Shift+k" = "move up";
-        "${modifier}+Shift+l" = "move right";
-
-        "${modifier}+s" = "layout stacking";
-        "${modifier}+w" = "layout tabbed";
-
-        "${modifier}+space" = "focus mod_toggle";
-        "${modifier}+Shift+space" = "floating toggle";
-
-        "${modifier}+Shift+r" = "restart";
-        "${modifier}+Shift+c" = "reload";
-      };
-
-      startup = [
-        {
-          command = "exec i3-msg workspace 1";
-          always = true;
-          notification = false;
-        }
-        {
-          command = "/usr/bin/feh --bg-scale ~/Pictures/catppuccin-wallpapers/misc/lonely-fish.png";
-          always = true;
-          notification = false;
-        }
-        {
-          command = "/usr/bin/setxkbmap us_qwerty-fr";
-          always = true;
-          notification = false;
-        }
-      ];
-    };
-  };
-
-  programs.i3status-rust = {
-    enable = true;
-    bars = {
-      top = {
-        blocks = [
+    xsession.windowManager.i3 = {
+      enable = true;
+      package = pkgs.i3-gaps;
+      config = rec {
+        window.commands = [
           {
-            block = "time";
-            interval = 60;
-            format = {
-              full = "$timestamp.datetime(f:'%a %d/%m/%Y %R', l:en_US) ";
-              short = "$timestamp.datetime(f:%R) ";
+            command = "floating enable, sticky enable, resize set 1000 700, border pixel 1";
+            criteria = {
+              class = "floating_shell";
+            };
+          }
+        ];
+        bars = [
+          {
+            fonts = {
+              names = ["${font}"];
+              size = 12.0;
+            };
+            position = "top";
+            statusCommand = "/usr/bin/i3status-rs /home/laurent/.config/i3status-rust/config-top.toml";
+            colors = rec {
+              background = "#${config.colorScheme.palette.base00}";
+              focusedBackground = "#${config.colorScheme.palette.base00}";
+              statusline = "#${config.colorScheme.palette.base00}";
+              focusedStatusline = statusline;
+              bindingMode = rec {
+                inherit background;
+                border = "#${config.colorScheme.palette.base08}";
+                text = border;
+              };
+              focusedWorkspace = {
+                background = "#${config.colorScheme.palette.base02}";
+                border = "#${config.colorScheme.palette.base08}";
+                text = "#${config.colorScheme.palette.base08}";
+              };
+              activeWorkspace = {
+                inherit background;
+                inherit (focusedWorkspace) text border;
+              };
+              inactiveWorkspace = rec {
+                inherit background;
+                border = "#${config.colorScheme.palette.base05}";
+                text = border;
+              };
+            };
+          }
+        ];
+
+        fonts = {
+          names = ["${font}"];
+          size = 12.0;
+
+        };
+        modifier = "Mod1";
+        gaps = {
+          inner = 10;
+          outer = 5;
+        };
+        colors = {
+          focused = {
+            border = "#${config.colorScheme.palette.base00}";
+            background = "#${config.colorScheme.palette.base01}";
+            text = "#${config.colorScheme.palette.base07}";
+            indicator = "#${config.colorScheme.palette.base00}";
+            childBorder = "#${config.colorScheme.palette.base08}";
           };
-         }
-       ];
+          unfocused = {
+            border = "#${config.colorScheme.palette.base00}";
+            background = "#${config.colorScheme.palette.base00}";
+            text = "#${config.colorScheme.palette.base03}";
+            indicator = "#${config.colorScheme.palette.base00}";
+            childBorder = "#${config.colorScheme.palette.base00}";
+          };
+          focusedInactive = {
+            border = "#${config.colorScheme.palette.base00}";
+            background = "#${config.colorScheme.palette.base00}";
+            text = "#${config.colorScheme.palette.base0F}";
+            indicator = "#${config.colorScheme.palette.base00}";
+            childBorder = "#${config.colorScheme.palette.base00}";
+          };
+          urgent = {
+            border = "#${config.colorScheme.palette.base00}";
+            background = "#${config.colorScheme.palette.base00}";
+            text = "#${config.colorScheme.palette.base04}";
+            indicator = "#${config.colorScheme.palette.base00}";
+            childBorder = "#${config.colorScheme.palette.base00}";
+          };
+        };
+        keybindings = lib.mkOptionDefault {
+          "XF86AudioMute" = "exec amixer set Master toggle";
+          "XF86AudioLowerVolume" = "exec amixer set Master 4%-";
+          "XF86AudioRaiseVolume" = "exec amixer set Master 4%+";
+          "XF86MonBrightnessDown" = "exec brightnessctl set 4%-";
+          "XF86MonBrightnessUp" = "exec brightnessctl set 4%+";
+          "${modifier}+Return" = "exec /usr/bin/alacritty";
+          "${modifier}+e" = "exec /usr/bin/emacsclient -nc";
+          "${modifier}+Shift+d" = "exec /usr/bin/rofi -show window";
+          "${modifier}+d" = "exec /usr/bin/rofi -modi drun -show drun";
+          "${modifier}+Shift+e" = "exec systemctl suspend";
+
+          "${modifier}+q" = "kill";
+
+          "${modifier}+h" = "focus left";
+          "${modifier}+j" = "focus down";
+          "${modifier}+k" = "focus up";
+          "${modifier}+l" = "focus right";
+
+          "${modifier}+Shift+h" = "move left";
+          "${modifier}+Shift+j" = "move down";
+          "${modifier}+Shift+k" = "move up";
+          "${modifier}+Shift+l" = "move right";
+
+          "${modifier}+s" = "layout stacking";
+          "${modifier}+w" = "layout tabbed";
+
+          "${modifier}+space" = "focus mod_toggle";
+          "${modifier}+Shift+space" = "floating toggle";
+
+          "${modifier}+Shift+r" = "restart";
+          "${modifier}+Shift+c" = "reload";
+        };
+
+        startup = [
+          {
+            command = "exec i3-msg workspace 1";
+            always = true;
+            notification = false;
+          }
+          {
+            command = "/usr/bin/feh --bg-scale ~/Pictures/catppuccin-wallpapers/misc/lonely-fish.png";
+            always = true;
+            notification = false;
+          }
+          {
+            command = "/usr/bin/setxkbmap us_qwerty-fr";
+            always = true;
+            notification = false;
+          }
+        ];
       };
     };
-  };
+
+    programs.i3status-rust = {
+      enable = true;
+      bars = {
+        top = {
+          blocks = [
+            {
+              block = "disk_space";
+              path = "/";
+              info_type = "available";
+              interval = 60;
+              warning = 20.0;
+              alert = 10.0;
+            }
+            {
+              block = "memory";
+              format = " $icon $mem_total_used_percents.eng(w:2) ";
+              format_alt = " $icon_swap $swap_used_percents.eng(w:2) ";
+            }
+            {
+              block = "cpu";
+              interval = 1;
+            }
+            {
+              block = "load";
+              interval = 1;
+              format = " $icon $1m ";
+            }
+            { block = "sound"; }
+            {
+              block = "bluetooth";
+              mac = "${bluetooth_mac_addr}";
+              click = [
+                {
+                  button = "left";
+                  cmd = "${bluetooth}";
+                }
+              ];
+            }
+            {
+              block = "time";
+              interval = 60;
+              click = [
+                {
+                  button = "left";
+                  cmd = "${calendar}";
+                }
+              ];
+              format = " $timestamp.datetime(f:'%a %d/%m %R') ";
+            }
+          ];
+          settings = {
+            theme =  {
+              theme = "solarized-dark";
+              overrides = {
+                idle_bg = "#${config.colorScheme.palette.base00}";
+                idle_fg = "#${config.colorScheme.palette.base05}";
+              };
+            };
+          };
+          icons = "awesome4";
+        };
+      };
+    };
 
 }
