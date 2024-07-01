102612ffc58f5b0270fba183dc1875d6776ee984
lejeunel <me@lejeunel.org>
2024-07-01 23:38:39 +0200

    systemd-boot

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -24,25 +24,10 @@
     ./hardware-configuration.nix
   ];
 
-  boot.loader = {
-          grub = {
-              enable                = true;
-              useOSProber           = true;
-              copyKernels           = true;
-              efiInstallAsRemovable = true;
-              efiSupport            = true;
-              fsIdentifier          = "label";
-              devices               = [ "nodev" ];
-              extraEntries = ''
-                  menuentry "Reboot" {
-                      reboot
-                  }
-                  menuentry "Poweroff" {
-                      halt
-                  }
-              '';
-          };
-      };
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.timeout = 2;
+
 
   nixpkgs = {
     # Configure your nixpkgs instance
