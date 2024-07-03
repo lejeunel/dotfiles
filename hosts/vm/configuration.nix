d0cfd167750ec55c1a697ed82919d7d6e134087c
lejeunel <me@lejeunel.org>
2024-07-03 17:20:59 +0200

    lightdm

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -24,6 +24,8 @@
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
     ../../users
+    ../../locale
+    ../../audio
   ];
 
   boot.loader.grub.enable = true;
@@ -35,33 +37,11 @@
 
   time.timeZone = "Europe/Paris";
 
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS =  "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.xterm.enable = true;
   services.xserver.windowManager.i3.enable = true;
 
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
 
   # for touchpad
   services.libinput.enable = true;
