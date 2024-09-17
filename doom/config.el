2dd40d1f5454dcbc23782d4ecc61f84552ec4f3a
lejeunel <me@lejeunel.org>
2024-09-17 05:49:04 +0200

    font sizes

--- doom/config.el
+++ doom/config.el
@@ -19,7 +19,7 @@
 
 (setq doom-theme 'doom-gruvbox)
 
-(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 20 :height 1.0 :weight 'normal))
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 22 :height 1.0 :weight 'normal))
 
 (setq highlight-indent-guides-auto-odd-face-perc 50)
 (setq highlight-indent-guides-auto-even-face-perc 50)
--- homeManagerModules/features/stylix/default.nix
+++ homeManagerModules/features/stylix/default.nix
@@ -30,6 +30,6 @@
     targets.rofi.enable = false;
     targets.waybar.enable = false;
 
-    fonts.sizes = { terminal = 14; };
+    fonts.sizes = { terminal = 13; };
   };
 }
