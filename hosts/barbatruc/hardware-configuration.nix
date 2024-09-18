dea8cd2aa8c641c310f3c0ec08a76c06f4876935
lejeunel <me@lejeunel.org>
2024-09-18 09:33:44 +0200

    kernel tty param and greetd

--- hosts/barbatruc/hardware-configuration.nix
+++ hosts/barbatruc/hardware-configuration.nix
@@ -4,31 +4,30 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.availableKernelModules =
+    [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
-  boot.kernelParams = ["i915.enable_psr=0"];
+  boot.kernelParams = [ "i915.enable_psr=0" "console=tty1" ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/47d6bb4b-dd85-4204-a08e-6359a67324af";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/47d6bb4b-dd85-4204-a08e-6359a67324af";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/2A63-EE1B";
-      fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/2A63-EE1B";
+    fsType = "vfat";
+    options = [ "fmask=0022" "dmask=0022" ];
+  };
 
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/a451d8bc-9257-4f78-92b6-31646a296f9a";
-      fsType = "ext4";
-    };
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/a451d8bc-9257-4f78-92b6-31646a296f9a";
+    fsType = "ext4";
+  };
 
   swapDevices = [ ];
 
@@ -41,5 +40,6 @@
   # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
