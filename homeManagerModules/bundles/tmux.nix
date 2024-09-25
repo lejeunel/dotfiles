9b2342c7ecb63e7f9639cf6e3a73e9a137bf26ba
lejeunel <me@lejeunel.org>
2024-09-25 11:10:37 +0200

    tmux

--- homeManagerModules/bundles/tmux.nix
+++ homeManagerModules/bundles/tmux.nix
@@ -12,16 +12,16 @@
     escapeTime = 0;
     keyMode = "vi";
     extraConfig = ''
-      set-option -g status-right ' #{prefix_highlight} "#{=21:pane_title}" %H:%M %d-%b-%y'
-      set-option -g status-left-length 20
-      set-option -g @prefix_highlight_fg '${config.lib.stylix.colors.base00}'
-      set-option -g @prefix_highlight_bg '${config.lib.stylix.colors.base0D}'
 
       # Vim style pane selection
       bind h select-pane -L
       bind l select-pane -R
 
       bind-key -r f run-shell "tmux neww ~/.local/scripts/tmux-sessionizer"
+
+      bind-key R run-shell ' \
+        tmux source-file ~/.config/tmux/tmux.conf > /dev/null; \
+        tmux display-message "sourced tmux config"'
 
       bind-key -T copy-mode-vi v send-keys -X begin-selection
       bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
