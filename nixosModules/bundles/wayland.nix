07a79f51d957d3d0f4d1151cdef0cc5f8ccc3752
lejeunel <me@lejeunel.org>
2025-09-21 06:29:39 +0200

    added libinput in wayland

--- nixosModules/bundles/wayland.nix
+++ nixosModules/bundles/wayland.nix
@@ -1,9 +1,14 @@
-{ pkgs, lib, ... }: {
+{ pkgs, ... }:
+{
   myNixOS.wayland.enable = true;
   myNixOS.bluetooth.enable = true;
   myNixOS.stylix.enable = true;
 
-  environment.systemPackages = with pkgs; [ nwg-displays brightnessctl ];
+  environment.systemPackages = with pkgs; [
+    nwg-displays
+    brightnessctl
+    libinput
+  ];
   xdg.portal = {
     enable = true;
     wlr.enable = true; # adds pkgs.xdg-desktop-portal-wlr to extraPortals
