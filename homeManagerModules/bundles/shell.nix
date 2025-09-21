8fc95f1cee043903eed2d6dacbf7e64fa6afb997
lejeunel <me@lejeunel.org>
2025-09-21 13:08:04 +0200

    bluetuith vim bindings

--- homeManagerModules/bundles/shell.nix
+++ homeManagerModules/bundles/shell.nix
@@ -109,5 +109,21 @@
     };
 
   };
+  xdg.configFile."bluetuith/bluetuith.conf".text = ''
+    {
+      adapter: ""
+      adapter-states: ""
+      connect-bdaddr: ""
+      gsm-apn: ""
+      gsm-number: ""
+      keybindings: {
+        NavigateDown: j
+        NavigateUp: k
+        Quit: q
+      }
+      receive-dir: ""
+      theme: {}
+    }
+  '';
 
 }
