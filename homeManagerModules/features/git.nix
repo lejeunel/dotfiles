a14c7064de531ea3d9aa049e4c63c23c9d6c0b1e
lejeunel <me@lejeunel.org>
2025-11-16 01:08:36 +0100

    formatting

--- homeManagerModules/features/git.nix
+++ homeManagerModules/features/git.nix
@@ -6,10 +6,6 @@
   home.packages = with pkgs; [
     lazygit
   ];
-  # trace: warning: The option `programs.git.aliases' defined in `/nix/store/ax1aln7na5fhdwzyqcjbdcw6chkbhnml-source/homeManagerModules' has been renamed to `programs.git.settings.alias'.
-  # trace: warning: The option `programs.git.userEmail' defined in `/nix/store/ax1aln7na5fhdwzyqcjbdcw6chkbhnml-source/homeManagerModules' has been renamed to `programs.git.settings.user.email'.
-  # trace: warning: The option `programs.git.userName' defined in `/nix/store/ax1aln7na5fhdwzyqcjbdcw6chkbhnml-source/homeManagerModules' has been renamed to `programs.git.settings.user.name'.
-  # trace: warning: The option `programs.git.extraConfig' defined in `/nix/store/ax1aln7na5fhdwzyqcjbdcw6chkbhnml-source/homeManagerModules' has been renamed to `programs.git.settings'.
 
   programs.git = {
     enable = true;
