093238714ef7dcd77d2d6f90c53e8a5b520303c3
lejeunel <me@lejeunel.org>
2026-01-05 14:31:14 +0100

    python pyqt6 backend for matplotlib

--- homeManagerModules/features/python.nix
+++ homeManagerModules/features/python.nix
@@ -1,8 +1,19 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    python313Packages.ipython
+  home.packages = [
+
+    pkgs.qt6.qtwayland
+    pkgs.mesa
+    pkgs.libGL
+    pkgs.libxkbcommon
+
+    (pkgs.python313.withPackages (ps: [
+      ps.matplotlib
+      ps.pyqt6
+      ps.ipython
+    ]))
+
   ];
 
   home.file.".ipython/profile_default/ipython_config.py".text = ''
@@ -10,4 +21,9 @@
     c.InteractiveShellApp.extensions = ["autoreload"]
     c.InteractiveShellApp.exec_lines = ["%autoreload 2"]
   '';
+
+  home.sessionVariables = {
+    MPLBACKEND = "QtAgg";
+  };
+
 }
