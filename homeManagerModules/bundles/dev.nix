4615cfe6312b8ca90d5c04a09f61fa7b6ebe5d30
lejeunel <me@lejeunel.org>
2026-01-06 13:40:09 +0100

    scheme/guile

--- homeManagerModules/bundles/dev.nix
+++ homeManagerModules/bundles/dev.nix
@@ -4,6 +4,7 @@
 
   myHomeManager.golang.enable = true;
   myHomeManager.cpp.enable = true;
+  myHomeManager.guile.enable = true;
   myHomeManager.lua.enable = true;
   myHomeManager.javascript.enable = true;
   myHomeManager.git.enable = true;
