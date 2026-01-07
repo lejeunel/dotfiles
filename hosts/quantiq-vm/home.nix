25468d68edaf24add6c907863028484ae8586b3f
lejeunel <me@lejeunel.org>
2026-01-07 10:32:32 +0100

    quantiq-vm disable stylix

--- hosts/quantiq-vm/home.nix
+++ hosts/quantiq-vm/home.nix
@@ -8,6 +8,7 @@
   imports = [ outputs.homeManagerModules.default ];
 
   myHomeManager = {
+    bundles.stylix.enable = false;
     bundles.shell.enable = true;
     bundles.dev.enable = true;
     bundles.encryption.enable = true;
