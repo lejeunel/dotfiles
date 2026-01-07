5873fe654860c7f8e0385a98b44bbcbc8504686a
lejeunel <me@lejeunel.org>
2026-01-07 10:27:02 +0100

    quantiq-vm: removed stylix and editors

--- hosts/quantiq-vm/home.nix
+++ hosts/quantiq-vm/home.nix
@@ -8,9 +8,7 @@
   imports = [ outputs.homeManagerModules.default ];
 
   myHomeManager = {
-    bundles.stylix.enable = true;
     bundles.shell.enable = true;
-    bundles.editors.enable = true;
     bundles.dev.enable = true;
     bundles.encryption.enable = true;
   };
