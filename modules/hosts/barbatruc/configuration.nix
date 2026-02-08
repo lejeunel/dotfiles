570677da99bb5f3ff14cb2393dca49eebb0617e8
lejeunel <me@lejeunel.org>
2026-02-08 09:04:22 +0100

    nix experimental features

--- modules/hosts/barbatruc/configuration.nix
+++ modules/hosts/barbatruc/configuration.nix
@@ -57,6 +57,10 @@
 
       # Allow unfree packages
       nixpkgs.config.allowUnfree = true;
+      nix.settings.experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
 
       # List packages installed in system profile. To search, run:
       # $ nix search wget
--- modules/hosts/tartopom/configuration.nix
+++ modules/hosts/tartopom/configuration.nix
@@ -68,14 +68,15 @@
 
       };
 
-      # Enable networking
       networking.networkmanager.enable = true;
 
-      # Set your time zone.
       time.timeZone = "Europe/Paris";
 
-      # Allow unfree packages
       nixpkgs.config.allowUnfree = true;
+      nix.settings.experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
 
       # List packages installed in system profile. To search, run:
       # $ nix search wget
